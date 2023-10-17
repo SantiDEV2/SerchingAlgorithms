177499d762a4decfb1652771da9c4bbc4d080baf
SantiDEV2 <100314893+SantiDEV2@users.noreply.github.com>
2023-10-16 19:02:11 -0600

    Test

--- Assets/Scripts/DetectCellLocation.cs
+++ Assets/Scripts/DetectCellLocation.cs
@@ -6,6 +6,10 @@ public class DetectCellLocation : MonoBehaviour
     private Vector3 worldPosition;
     public GridLayout gridLayout;
     public Tilemap tilemap;
+    public TileBase origin;
+    public TileBase Destino;
+    public TileBase Offset;
+    private TileBase actualTile;
 
     private void Update()
     {
@@ -14,17 +18,24 @@ public class DetectCellLocation : MonoBehaviour
         worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
         Vector3Int cellPosition = gridLayout.WorldToCell(worldPosition);
         mousePos = gridLayout.CellToWorld(cellPosition);
+        cellPosition.z = 0;
 
         if (Input.GetMouseButtonDown(0))
         {
-
-            Debug.Log("Origen" + cellPosition);
+           /*  Debug.Log("Origen" + cellPosition); */
+            actualTile = tilemap.GetTile(cellPosition);
+            if(actualTile == null) { return; }
+            tilemap.SetTile(cellPosition, origin);
+            var lastPosition = cellPosition;
         }
 
         if (Input.GetMouseButtonDown(1))
         {
-            Debug.Log(cellPosition);
-            var actualTile = tilemap.GetTile(cellPosition);
+            Debug.Log("Destino" + cellPosition);
+            actualTile = tilemap.GetTile(cellPosition);
+            if(actualTile == null) { return; }
+            tilemap.SetTile(cellPosition, Destino);
         }
     }
+
 }
